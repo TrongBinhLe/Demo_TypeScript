c435f2fd287dafe372dd05c5b3bb9c4345081fda
Trong Binh Le <ltbinhbk2010@gmail.com>
2023-08-30 14:06:58 +0700

    Research the Promise in Typescripts

--- app.js
+++ app.js
@@ -1,14 +1,36 @@
-function combine(input1, input2) {
+function combine(input1, input2, resultConversion) {
     var result;
-    if (typeof input1 === "number" && typeof input2 === "number") {
-        result = input1 + input2;
+    if ((typeof input1 === "number" && typeof input2 === "number") ||
+        resultConversion === "as-number") {
+        result = +input1 + +input2;
     }
     else {
         result = input1 + " " + input2;
     }
     return result;
 }
-var combineAges = combine(30, 60);
+var combineAges = combine(30, 60, "as-number");
 console.log(combineAges);
-var combineNames = combine("MAX", "Anana");
+var combineNames = combine("MAX", "Anana", "as-text");
 console.log(combineNames);
+var combineAgeWithString = combine("23", "232", "as-text");
+console.log(combineAgeWithString);
+var promise4 = new Promise(function (resolve, reject) {
+    var result = false;
+    if (result) {
+        setTimeout(function () {
+            resolve("Resolve state"), 1000;
+        });
+    }
+    else {
+        reject("Rejected state");
+    }
+});
+// Understading promise
+promise4.then(function (value) {
+    console.log("This will run as it is a resolved promise, The resolved value is", value);
+});
+promise4.catch(function (reason) {
+    console.log("This will not run as it is a resolved promise", reason);
+});
+// Understanding async-await
--- app.ts
+++ app.ts
@@ -1,15 +1,50 @@
-function combine(input1: string | number, input2: string | number) {
+function combine(
+  input1: string | number,
+  input2: string | number,
+  resultConversion: "as-number" | "as-text"
+) {
   let result;
-  if (typeof input1 === "number" && typeof input2 === "number") {
-    result = input1 + input2;
+  if (
+    (typeof input1 === "number" && typeof input2 === "number") ||
+    resultConversion === "as-number"
+  ) {
+    result = +input1 + +input2;
   } else {
     result = input1 + " " + input2;
   }
   return result;
 }
 
-const combineAges = combine(30, 60);
+const combineAges = combine(30, 60, "as-number");
 console.log(combineAges);
 
-const combineNames = combine("MAX", "Anana");
+const combineNames = combine("MAX", "Anana", "as-text");
 console.log(combineNames);
+
+const combineAgeWithString = combine("23", "232", "as-text");
+console.log(combineAgeWithString);
+
+const promise4 = new Promise((resolve, reject) => {
+  const result = false;
+  if (result) {
+    setTimeout(function () {
+      resolve("Resolve state"), 1000;
+    });
+  } else {
+    reject("Rejected state");
+  }
+});
+
+// Understading promise
+promise4.then(function (value) {
+  console.log(
+    "This will run as it is a resolved promise, The resolved value is",
+    value
+  );
+});
+
+promise4.catch(function (reason) {
+  console.log("This will not run as it is a resolved promise", reason);
+});
+
+// Understanding async-await
