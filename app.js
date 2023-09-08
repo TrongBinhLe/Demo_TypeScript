2c2a488d00e86ee7511d315360091ba49481e657
Trong Binh Le <ltbinhbk2010@gmail.com>
2023-09-08 14:35:51 +0700

    Understading typescript core libs

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 "use strict";
 console.log("Trong Binh");
 const userName = "Trong Binh Le";
+const button = document.querySelector('button');
+button === null || button === void 0 ? void 0 : button.addEventListener("click", () => {
+    console.log("Cliked");
+});
--- app.ts
+++ app.ts
@@ -1,2 +1,7 @@
 console.log("Trong Binh");
 const userName = "Trong Binh Le"
+const button = document.querySelector('button')
+
+button?.addEventListener("click", () => {
+    console.log("Cliked");
+})
