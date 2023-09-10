8ed28b1e5d0aa06c65aaceb4dc475ebb3bdb5596
TrongBinhLe <ltbinhbk2010@gmail.com>
2023-09-10 07:40:32 +0700

    The Spread Operator

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,19 @@ console.log("Trong Binh");
 const userName = "Trong Binh Le ";
 const button = document.querySelector("button");
 
-if(button) {
-  button.addEventListener('click', () => {
+if (button) {
+  button.addEventListener("click", () => {
     console.log("Cliked");
   });
 }
+
+const hobbies = ["Sports", "Watching machine"];
+const activieHobbies = ["Hikings"];
+activieHobbies.push(...activieHobbies);
+
+const people = {
+  name: "Trong Binh",
+  age: 23,
+};
+
+const copiedPeople = { ...people };
