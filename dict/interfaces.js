6971a8e47cdca160a5ab57aab082874e8e2a8216
TrongBinhLe <ltbinhbk2010@gmail.com>
2023-09-16 14:42:35 +0700

    Interface skill with typeScript

---
+++ dict/interfaces.js
@@ -0,0 +1,19 @@
+"use strict";
+let addFunc;
+addFunc = (n1, n2) => {
+    return n1 + n2;
+};
+class Person {
+    constructor(n) {
+        this.age = 30;
+        this.name = n;
+    }
+    greet(phrase) {
+        console.log(phrase + " " + this.name);
+    }
+}
+let user1;
+user1 = new Person("Max");
+user1.greet("Hi there - I am");
+console.log(user1);
+//# sourceMappingURL=interfaces.js.map
