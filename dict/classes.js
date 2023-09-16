ae968a5a8ea0bf091bebec3ff7198a6f03cb1d6d
TrongBinhLe <ltbinhbk2010@gmail.com>
2023-09-16 14:36:17 +0700

    AdvantcedTypes

---
+++ dict/classes.js
@@ -0,0 +1,78 @@
+"use strict";
+class Department {
+    constructor(id, name) {
+        this.id = id;
+        this.name = name;
+        this.employees = [];
+    }
+    describe() {
+        console.log(`Department (${this.id}): ${this.name}`);
+    }
+    static createEmployee(name) {
+        return { name: name };
+    }
+    addEmployee(employee) {
+        this.employees.push(employee);
+    }
+    printEmployeeInformation() {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    }
+}
+Department.modelYear = 2023;
+class ITDepartment extends Department {
+    constructor(id, admins) {
+        super(id, "IT");
+        this.admins = admins;
+    }
+}
+class AccountingDepartment extends Department {
+    get mostRecentReport() {
+        if (this.lastReport) {
+            return this.lastReport;
+        }
+        else {
+            throw new Error();
+        }
+    }
+    set mostRecentReport(value) {
+        if (!value) {
+            throw new Error("Please pass in a valid value!");
+        }
+        this.addReport(value);
+    }
+    constructor(id, reports) {
+        super(id, "Accounting");
+        this.reports = reports;
+        this.lastReport = reports[0];
+    }
+    addEmployee(employee) {
+        if (this.name == "Max") {
+            return;
+        }
+        this.employees.push(employee);
+    }
+    addReport(text) {
+        this.reports.push(text);
+    }
+    printReports() {
+        console.log(this.reports);
+    }
+}
+const it = new ITDepartment("d1", ["Max"]);
+it.addEmployee("Max");
+it.addEmployee("Manu");
+it.describe();
+it.name = "NEW NAME";
+it.printEmployeeInformation();
+console.log(it);
+console.log(Department.createEmployee("Bcons Suoi Tien"));
+const accounting = new AccountingDepartment("d2", ["TrongBInh"]);
+accounting.mostRecentReport = "Year End Report";
+accounting.addReport("Something went wrong...");
+console.log("Log:" + accounting.mostRecentReport);
+accounting.addEmployee("Max");
+accounting.addEmployee("TrongBinh");
+accounting.printReports();
+accounting.printEmployeeInformation();
+//# sourceMappingURL=classes.js.map
