adf4f213ee6a42607b01fdad1642d31a567d2156
TrongBinhLe <ltbinhbk2010@gmail.com>
2023-11-01 08:45:33 +0700

    Section7: Generic Classs

--- dict/generic/generic.js
+++ dict/generic/generic.js
@@ -19,4 +19,29 @@ function extractAndConvert(obj, key) {
     return 'Value: ' + obj[key];
 }
 extractAndConvert({ name: 'MAZ' }, 'name');
+class DataStorage {
+    constructor() {
+        this.data = [];
+    }
+    addItem(item) {
+        this.data.push(item);
+    }
+    removeItem(item) {
+        this.data.splice(this.data.indexOf(item), 1);
+    }
+    getItems() {
+        return [...this.data];
+    }
+}
+const textStorage = new DataStorage();
+textStorage.addItem('MAX');
+textStorage.addItem('MAnu');
+textStorage.removeItem('MAX');
+console.log('Generic class:', textStorage.getItems());
+const numberStorage = new DataStorage();
+const objStorage = new DataStorage();
+objStorage.addItem({ name: 'MAZ' });
+objStorage.addItem({ name: 'ZORO' });
+objStorage.removeItem({ name: 'MAZ' });
+console.log('Generic class object:', objStorage.getItems());
 //# sourceMappingURL=generic.js.map
--- src/generic/generic.ts
+++ src/generic/generic.ts
@@ -29,3 +29,37 @@ function extractAndConvert<T extends Object, U extends keyof T>(obj: T, key: U) 
 }
 
 extractAndConvert({name: 'MAZ'}, 'name');
+
+// Generic Class 
+
+class DataStorage <T>{
+    private readonly data: T[] = [];
+
+    addItem(item: T) { 
+        this.data.push(item)
+    }
+
+    removeItem(item: T) {
+        this.data.splice(this.data.indexOf(item), 1)
+    }
+
+    getItems() {
+        return [...this.data];
+    }
+}
+
+
+const textStorage = new DataStorage<string>();
+textStorage.addItem('MAX');
+textStorage.addItem('MAnu');
+textStorage.removeItem('MAX');
+console.log('Generic class:', textStorage.getItems())
+
+
+const numberStorage = new DataStorage<number>();
+const objStorage = new DataStorage<object>();
+objStorage.addItem({name: 'MAZ'});
+objStorage.addItem({name: 'ZORO'});
+objStorage.removeItem({name: 'MAZ'});
+
+console.log('Generic class object:', objStorage.getItems());
